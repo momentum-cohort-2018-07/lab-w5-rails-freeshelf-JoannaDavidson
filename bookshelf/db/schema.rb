9fd840a654036b8a2d0719134689b4bba5e6027e
JoannaDavidson <joanna@shrikesong.com>
2018-08-28 14:26:23 -0400

    created new model, migrated db

--- bookshelf/db/schema.rb
+++ bookshelf/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_27_185049) do
+ActiveRecord::Schema.define(version: 2018_08_28_180827) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -21,6 +21,16 @@ ActiveRecord::Schema.define(version: 2018_08_27_185049) do
     t.string "format"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "city"
+    t.integer "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_users_on_book_id"
   end
 
 end
